7d0d5a1a85f5b655d7e1f6b2b7057ba7374f73b5
mike2151 <redefiningproductions@gmail.com>
2018-02-12 15:51:25 -0500

    completed the first version of extension

--- editor.js
+++ editor.js
@@ -1,7 +1,7 @@
 function clickaction() {
-  document.getElementById('instructions').innerHTML = "Click anywhere on the page to insert your code";
+  document.getElementById('instructions').innerHTML = "Click anywhere on the page to insert your code.<br/>Note: if you do not see anything, the current website has blocked direct code injections.<br/> To stop the injections, open the chrome extension again.";
   chrome.tabs.executeScript({
-        code: 'var code_to_inject = "'  + String(document.getElementById("editor_box").value) + '"'
+        code: 'var code_to_inject = `'  + String(document.getElementById("editor_box").value) + '`;'
     }, function(result) {
         chrome.tabs.executeScript({file: "insert.js"}, function(result) {
         });
